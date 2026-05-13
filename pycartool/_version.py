5491a4399cd70539d8106c48cc9371c7d2e6a8af
Victor Férat <victor.ferat@live.Fr>
2026-05-13 17:53:49 +0200

    Update docs

--- pycartool/_version.py
+++ pycartool/_version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = '0.1.0.dev138+gbbbb68fe5.d20260513'
-__version_tuple__ = version_tuple = (0, 1, 0, 'dev138', 'gbbbb68fe5.d20260513')
+__version__ = version = '0.1.0.dev139+gd8f266ade.d20260513'
+__version_tuple__ = version_tuple = (0, 1, 0, 'dev139', 'gd8f266ade.d20260513')
 
-__commit_id__ = commit_id = 'gbbbb68fe5'
+__commit_id__ = commit_id = 'gd8f266ade'

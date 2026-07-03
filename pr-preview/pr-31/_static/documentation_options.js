d1a84c7d444de4beebfdf797da48ffde1e499e52
vferat <vferat@users.noreply.github.com>
2026-07-03 12:06:20 +0000

    Deploy preview for PR 31 🛫

--- pr-preview/pr-31/_static/documentation_options.js
+++ pr-preview/pr-31/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.1.0.dev139+g098efcdc9',
+    VERSION: '0.1.0.dev141+g5c55e08bd',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
